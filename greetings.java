1cf340ada58498e958bc701bb33cfcb9e58be1dc
rommel <romel.jefrin@gmail.com>
2024-02-18 23:21:46 -0500

    made changes accordingly

--- greetings.java
+++ greetings.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <<this is code change after git stash 202>>
 class second{
 
@@ -5,3 +6,10 @@ int j=100000;
 int k=200000;
 
 }
+=======
+<<this code is from 101 before git stash>>
+class demo{
+a=10;
+b=20;
+}
+>>>>>>> Stashed changes
